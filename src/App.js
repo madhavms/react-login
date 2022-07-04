7b125847e1a9cd18f4930522fd09a363a46af82f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-07-04 13:05:26 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Register from './components/Register';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './components/Home';
 
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Register/>}/>
+      <Route path="/home" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
